46507045bf74741d247ee9e51d8f038d0cde6df9
pedrolucassiqueiradasilva4-pixel <pedrolucassiqueiradasilva4@gmail.com>
2025-10-19 20:04:08 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
+// Configuração do Vite
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
+  root: './',
   build: {
     outDir: 'dist',
-  }
+    rollupOptions: {
+      input: './index.html',
+    },
+  },
 })
+
