6cb84d100ae55da1f6b4665d911f86aa304e6bb8
pedrolucassiqueiradasilva4-pixel <pedrolucassiqueiradasilva4@gmail.com>
2025-10-19 19:43:06 -0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.',
-  build: {
-    outDir: 'dist',
-  },
   resolve: {
     alias: {
-      '@': '/src',
+      '@': resolve(__dirname, 'src'),
     },
   },
+  build: {
+    outDir: 'dist',
+  }
 })
